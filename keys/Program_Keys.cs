8b2b2ae5b0cbe5da2106845fb44615d37beafb97
jpechacek80 <jpechacek@yahoo.com>
2020-06-10 12:29:17 -0500

    Add project files.

---
+++ keys/Program_Keys.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace keys
+{
+  class Program_Keys
+  {
+    static void Main(string[] args)
+    {
+      string str = "";
+      ConsoleKeyInfo c = new ConsoleKeyInfo();
+      
+      while (c.Key != ConsoleKey.Escape)
+      {
+         c = Console.ReadKey();
+        str += c.KeyChar;
+      }
+      
+      Console.WriteLine("\n"+str);
+      Console.ReadKey();
+
+    }
+  }
+}
---
+++ test_baseclass/Program_Base.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace test_baseclass
+{
+  class Program_Base
+  {
+    static void Main(string[] args)
+    {
+
+      var o= new Person();
+      char c;
+      do
+      {
+        c = Console.ReadKey().KeyChar;
+
+        if (c == 'e')
+          o = new Employee();
+        else
+          o = new Customer();
+         
+         o.GetInfo();
+
+      } while (c != 0x1B);
+
+      Employee E = new Employee();
+      E.GetInfo();
+      Customer C = new Customer();
+      C.GetInfo();
+
+      Console.ReadKey();
+    }
+  }
+  public class Person
+  {
+    protected string ssn = "444-55-6666";
+    protected string name = "John Smith";
+
+    public virtual void GetInfo()
+    {
+      Console.WriteLine("Name: {0}", name);
+      Console.WriteLine("SSN: {0}", ssn);
+    }
+  }
+  class Employee : Person
+  {
+    public string id = "ABC567EFG";
+    public override void GetInfo()
+    {
+      base.GetInfo();            // Calling the base class GetInfo method:
+      Console.WriteLine("Employee ID: {0}", id);
+    }
+  }
+  class Customer : Person
+  {
+    public string id = "ABC123456";
+    public override void GetInfo()
+    {
+      base.GetInfo();            // Calling the base class GetInfo method:
+      Console.WriteLine("Customer ID: {0}", id);
+    }
+  }
+}
